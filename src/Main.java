072582adac7fd3fb8f4b3dc40e826060ce561fda
MartinelliNTZ <1821669@aluno.univesp.br>
2022-10-19 21:51:03 -0300

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.List;
+
 public class Main {
+   // List<Funcionarios> funcionarios[];
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Funcionarios funcionarios =new Funcionarios(15,"Matheus", "matheus@com");
+
     }
 }
